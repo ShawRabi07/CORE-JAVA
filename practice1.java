4d5064fb6b4b0b31ff4e8d9f2ead260e77e3c3d7
ShawRabi07 <rshaw86309@gmail.com>
2026-02-02 14:40:05 +0530

    1st day at java pep class

---
+++ practice1.java
@@ -0,0 +1,186 @@
+// class Student{
+//     public static void main(String[] args){
+
+//         // long rabi = 23223232232L;
+//         // float n = 2.2F;
+//         // // System.out.println("Hey Bittu ");
+
+//         // System.out.println(n);
+//         // System.out.println(rabi);
+
+//         // int a;
+//         // // System.out.println(a);
+//         // Student n = null;
+//         // System.out.println(n);
+
+        
+//     }
+// }
+
+
+
+// class Student{
+//     public static void main(String[] args) {
+//     System.out.println("Hello World");
+//     System.out.println();
+//     System.out.print(); //error
+//     }
+// }
+
+// class Datatype{
+//     public static void main(String [] args){
+//         long a = 1000001234567890L;
+//         float b = 10.5f;
+//         double c = 20.99d;
+//         int d = 100;
+
+//         System.out.println(a);
+//         System.out.println(b);
+//         System.out.println(c);
+//         System.out.println(d);
+//     }
+// }
+
+// class Book{
+//     public static void main(String[]args){
+//         int a;  //error
+//         System.out.println(a);
+//     }
+// }
+
+// class TypeCasting{  //convert vaule into different data type
+//     public static void main(String[]args){
+//         int a = 10;
+//         double d = a; //implicit type casting
+//         System.out.println(d);
+//     }
+// }
+
+// class TypeCasting{  //convert vaule into different data type
+//     public static void main(String[]args){
+//         double d = 10.5; 
+//         int num = (int)d; //explicit type casting
+//         System.out.println(num);
+//     }
+// }
+
+// class TypeCasting{  //convert vaule into different data type
+//     public static void main(String[]args){
+//         char c = 'a';
+//         int a = c; //explicit type casting
+//         System.out.println(a);
+//         double d = 89;
+//         char j = (char)d;
+//         System.out.println(j);
+
+//     }
+// }
+
+
+// class Operators{
+//     public static void main(String[] args){
+//         int a = 10, b = 20;
+
+//         String c = (a > b) ? "a is greater" : "b is greater";
+//         System.out.println(c);
+//     }
+// }
+
+
+
+
+// Assessment
+
+// import java.util.Scanner;
+
+// public class StudentEligibility {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+  
+//         System.out.print("Enter marks: ");
+//         int marks = sc.nextInt();
+
+//         System.out.print("Enter attendance percentage: ");
+//         int attendance = sc.nextInt();
+
+//         System.out.println((marks > 0 && attendance >0) ? "true" : "false");
+       
+//         boolean eligible = (marks >= 40) && (attendance > 75);
+        
+//         String result = eligible ? "Pass" : "Fail";
+//         // if (eligible) {
+//         //     marks += 5;  
+//         // }
+
+//         marks = eligible ? (marks += 5) : marks;
+
+
+//         System.out.println("Result: " + result);
+//         System.out.println("Final Marks: " + marks);
+
+//        sc.close();
+//     }
+// }
+
+
+// public class practice1 {
+
+//     public static void main(String[] args) {
+
+//         char character = '1';
+//         String result;
+
+//         if(character >= 'A' && character <= 'Z'){
+//             result = "Uppercase";
+
+//         }
+//         else if(character >= 'a' && character <= 'z'){
+//             result = "LowerCase";
+//         }
+//         else if(character >= '0' && character <= '9'){
+//             result = "Digit";
+//         }
+//         else{
+//             result = "Special Char";
+//         }
+
+//         System.out.println("result: "+ result);
+        
+//     }
+    
+// }
+
+
+
+// public class practice1 {
+
+//     public static void main(String[] args) {
+
+//         int a = 50;
+//         int b= 70;
+//         int c = 40;
+
+//         int largest;
+        
+//         if(a>b && a>c){
+//             largest = a;
+//         }
+//         else if(b>a && b>c){
+//             largest = b;
+//         }
+//         else{
+//             largest = c;
+//         }
+
+//         System.out.println("Largest Num: " + largest);
+        
+//     }
+    
+// }
+
+
+class practice1{
+
+    
+}
+
