3f0e701641378bd62cf51b080c7af707e0c38216
ShawRabi07 <rshaw86309@gmail.com>
2026-02-12 22:59:17 +0530

    Intial commit

---
+++ LinkL.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+
+// import javax.swing.ScrollPaneLayout;
+public class LinkL {
+    public static void main(String[] args) {
+        // LinkedList l = new LinkedList();
+        // l.add(10);
+        // l.add("Hi");
+        // l.add(20);
+        // l.add(30);
+        // l.add("Hello");
+        // l.add(100);
+        
+        // for(Object o : l){
+        //     System.out.println(o);
+        // }
+        // System.out.println("********");
+
+        // LinkedList<String> l1 = new LinkedList<>();
+        //     l1.add("CSE");
+        //     l1.add("BCA");
+        //     l1.add("ME");
+        //     l1.add("ECE");
+
+        //     Collections.sort(l1);
+
+        //     for(String s :l1){
+        //         System.out.println(s);
+        //     }
+
+        // Stack s = new Stack<>();
+        // s.add(23)
+
+
+        // PriorityQueue<Integer> pq = new PriorityQueue<>();
+        // pq.add(23);
+        // pq.add(43);
+        // pq.add(75);
+
+        // System.out.println("Priority Queue");
+
+        // while(!pq.isEmpty()){
+        //     System.out.println(pq.poll());
+        // }
+
+
+        TreeMap<Integer, String> map = new TreeMap<>();
+
+        map.put(233, "sdasd");
+         map.put(23, "rabid");
+          map.put(2, "nextr");
+          System.out.println(map);
+
+    }
+}
